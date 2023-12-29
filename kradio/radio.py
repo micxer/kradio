461f0dcaa3c3703158685962603257de05085ae7
micxer <micxer@micxer.de>
2023-12-29 11:54:22 +0100

    Skeleton class

--- kradio/radio.py
+++ kradio/radio.py
@@ -3,3 +3,25 @@
 # This file is part of KRadio
 # Copyright (c) 2023 Michael Weinrich
 
+class KRadio():
+    """
+    KRadio main class
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        pass
+
+    def run(self):
+        """
+        Start KRadio
+        """
+        pass
+
+    def stop(self):
+        """
+        Stop KRadio
+        """
+        pass
